d1e04863979be2f7adec26a46eb4b41a5872f22b
Pedro Luka <pedrolukagt@gmail.com>
2025-08-12 17:14:03 -0300

    criando primeiras rotas

--- src/server.js
+++ src/server.js
@@ -1,11 +1,24 @@
 import express from 'express'
-const app = express()
-const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const app = express()
+const port = 5000
+
+app.get("/", (req, res) => {
+  res.send("Servidor rodando!");
+});
+
+app.post('/user' , (req, res) => {
+  res.json({message: 'Usuário criado com sucesso!'})
+})
+
+app.put('/user' , (req, res) => {
+  res.json({message: 'Usuário atualizado com sucesso!'})
+})
+
+app.delete('/user' , (req, res) => {
+  res.json({message: 'Usuário deletado com sucesso!'})
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port https://localhost:${port}`)
+  console.log(`Example app listening on port http://localhost:${port}`)
 })

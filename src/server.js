d59cef97c9c36f3e3126452c1ccd693ebfcfc00a
Pedro Luka <pedrolukagt@gmail.com>
2025-08-12 17:49:35 -0300

    usando express.json

--- src/server.js
+++ src/server.js
@@ -3,16 +3,35 @@ import express from 'express'
 const app = express()
 const port = 5000
 
+app.use(express.json())
+
 app.get("/", (req, res) => {
   res.send("Servidor rodando!");
 });
 
+// CRUD de Usuário
 app.post('/user' , (req, res) => {
-  res.json({message: 'Usuário criado com sucesso!'})
+  const dados = req.body
+  console.log(dados)
+
+  res.json({
+    message: 'Usuário criado com sucesso!',
+    profile: dados
+  })
+})
+
+app.get('/user' , (req, res) => {
+  res.json({message: 'Usuário encontrado com sucesso!'})
 })
 
 app.put('/user' , (req, res) => {
-  res.json({message: 'Usuário atualizado com sucesso!'})
+  const dados = req.body
+  console.log(dados)
+
+  res.json({
+    message: 'Usuário atualizado com sucesso!',
+    profile: dados
+  })
 })
 
 app.delete('/user' , (req, res) => {

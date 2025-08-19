3ef118ea5d853190941f2295736f01516f863683
Pedro Luka <pedrolukagt@gmail.com>
2025-08-19 15:41:21 -0300

    configurando server.js

--- src/server.js
+++ src/server.js
@@ -1,42 +1,19 @@
 import express from 'express'
 
+// Importando os Routers
+import userRouter from './routers/userRouter.js';
+import productRouter from './routers/productRouter.js';
+import supplierRouter from './routers/supplierRouter.js';
+
 const app = express()
 const port = 5000
 
 app.use(express.json())
 
-app.get("/", (req, res) => {
-  res.send("Servidor rodando!");
-});
-
-// CRUD de Usuário
-app.post('/user' , (req, res) => {
-  const dados = req.body
-  console.log(dados)
-
-  res.json({
-    message: 'Usuário criado com sucesso!',
-    profile: dados
-  })
-})
-
-app.get('/user' , (req, res) => {
-  res.json({message: 'Usuário encontrado com sucesso!'})
-})
-
-app.put('/user' , (req, res) => {
-  const dados = req.body
-  console.log(dados)
-
-  res.json({
-    message: 'Usuário atualizado com sucesso!',
-    profile: dados
-  })
-})
-
-app.delete('/user' , (req, res) => {
-  res.json({message: 'Usuário deletado com sucesso!'})
-})
+// Usando Routers
+app.use('/user', userRouter);
+app.use('/product', productRouter);
+app.use('/supplier', supplierRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)

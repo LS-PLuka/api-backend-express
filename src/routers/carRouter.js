4153158010234212613d85a0976547e8a9f68017
Pedro Luka <pedrolukagt@gmail.com>
2025-08-19 15:59:26 -0300

    car router

---
+++ src/routers/carRouter.js
@@ -0,0 +1,44 @@
+import express from 'express';
+
+const router = express.Router();
+
+// CRUD
+router.post('/', (req, res) => {
+    const dados = req.body;
+    console.log(dados);
+
+    res.json({
+        message: 'Carro criado com sucesso!',
+        car: dados
+    });
+})
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    res.json({ message: `Carro ${id} encontrado com sucesso!` });
+})
+
+router.put('/:id', (req, res) => {
+    const dados = req.body;
+    const { id } = req.params;
+    const marca = dados.marca;
+    const modelo = dados.modelo;
+
+    res.json({
+        message: `${marca} ${modelo} com ${id} atualizado com sucesso!`,
+        modelo: modelo,
+        marca: marca
+    });
+})
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    res.json({ message: `Carro ${id} deletado com sucesso!` });
+})
+
+router.get('/', (req, res) => {
+    res.json({ message: 'Lista de carros' });
+})
+
+// Exportando o Router
+export default router;
--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import express from 'express'
 import userRouter from './routers/userRouter.js';
 import productRouter from './routers/productRouter.js';
 import supplierRouter from './routers/supplierRouter.js';
+import carRouter from './routers/carRouter.js';
 
 const app = express()
 const port = 5000
@@ -14,6 +15,7 @@ app.use(express.json())
 app.use('/user', userRouter);
 app.use('/product', productRouter);
 app.use('/supplier', supplierRouter);
+app.use('/car', carRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
